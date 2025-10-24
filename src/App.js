41299386755ad7da42e04d208babbb3c3448c98d
JetProc <soonjae8297@gmail.com>
2025-10-24 16:56:16 +0900

    feat(Controller): implement App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import Race from './domain/Race.js';
+import OutputView from './view/OutputView.js';
+import InputView from './view/InputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNamesInput = await InputView.inputCarNames();
+      const tryCount = await InputView.inputTryCount();
+
+      //유효성 검증
+      const carNames = carNamesInput.split(',');
+      const race = new Race(carNames);
+
+      OutputView.printResultHeader();
+
+      for (let _ = 0; _ < tryCount; _++) {
+        race.playRound();
+      }
+
+      const winners = race.getWinners();
+      OutputView.printWinners(winners);
+    } catch (error) {
+      OutputView.printError(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
